aa56fccc10b740392e2976ffbe45ab4647e6dea3
Alexey Reznikov <oligsei.reznikov@gmail.com>
2017-11-08 21:21:18 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import './App.css';
+
+const logo: string = require('./logo.svg');
+
+export class App extends React.Component {
+    public render(): JSX.Element {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.tsx</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
+}
