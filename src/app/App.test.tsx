aa7800e65e2ed034fe84a115ab8f5571970eebf8
Alexey Reznikov <oligsei.reznikov@gmail.com>
2017-11-09 01:19:46 +0200

    fixed test

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import {HashRouter} from 'react-router-dom';
 import {App} from './App';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App/>, div);
+    ReactDOM.render(<HashRouter><App/></HashRouter>, div);
 });
