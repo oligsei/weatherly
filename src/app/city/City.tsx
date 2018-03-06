5b85da5d5774185c139caefa1650848032f46f16
Alexey Reznikov <oligsei.reznikov@gmail.com>
2018-03-06 22:32:06 +0200

    removed caching from index

--- src/app/city/City.tsx
+++ src/app/city/City.tsx
@@ -3,6 +3,6 @@ import * as React from 'react';
 export class City extends React.Component {
 
     public render(): React.ReactNode {
-        return (<span>Cities?!</span>);
+        return (<span>Cities?</span>);
     }
 }
